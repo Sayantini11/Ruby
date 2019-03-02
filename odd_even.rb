cd547683da4b85472719d03a45cdd4c67cc5afe4
Sayantini Das <santysayantini@gmail.com>
2019-03-02 14:04:12 +0530

    Odd even program implemented

---
+++ odd_even.rb
@@ -0,0 +1,6 @@
+puts "Enter a number"
+num = gets.chomp.to_i
+(num%2 == 0) ? (puts "The number is even") : (puts "The number is odd")
+#puts "The number is even" if (num%2 == 0)
+#num%2 == 0 && (puts "The number is even")
+
